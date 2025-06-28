402fd552113e47c8b97758415f35a5723665a3e4
DPeeta <dayronalejandrocruzpita@gmail.com>
2025-06-28 15:12:38 -0400

    same

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "/src/scss/_variables.scss";`,
+        additionalData: `@use "/src/scss/_variables.scss";`,
       },
     },
   },
